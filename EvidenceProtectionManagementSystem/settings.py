25080d930d1443a7de4688ab670f63492a88eb91
Aditya Kumar Maurya <adityakm79@gmail.com>
2025-04-16 22:41:49 +0530

    staticfiles added

--- EvidenceProtectionManagementSystem/settings.py
+++ EvidenceProtectionManagementSystem/settings.py
@@ -30,9 +30,9 @@ TEMPLATE_DIR = BASE_DIR / "templates"
 SECRET_KEY = "django-insecure-#ff(@6h=%67_fd!aedvg3+*4jc43goo)ub2(qf@#-1bn@mr-p&"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*', '.railway.app']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -141,9 +141,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_DIR =  os.path.join(BASE_DIR / "static")
+# STATIC_DIR =  os.path.join(BASE_DIR / "static")
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [STATIC_DIR]
+STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
+# STATICFILES_DIRS = [STATIC_DIR]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
